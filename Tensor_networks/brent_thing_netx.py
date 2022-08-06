b518c46365eaf5dfcfdc0df9164fd62f2a506242
joegibs <102627371+joegibs@users.noreply.github.com>
2022-08-06 15:56:54 -0400

    a different way to get the same answer

--- Tensor_networks/brent_thing_netx.py
+++ Tensor_networks/brent_thing_netx.py
@@ -9,13 +9,15 @@ import networkx as nx
 import itertools
 import numpy as np
 import matplotlib.pyplot as plt
+import copy
+import functools
 
 #%%
 pauli = {
-    "I": np.array(np.mat("1,0;0,1")),
-    "Z": np.array(np.mat("1,0;0,-1")),
-    "X": np.array(np.mat("0,1;1,0")),
-    "Y": np.array(1j * np.mat("0,-1;1,0")),
+    "I": np.array(np.mat("1+0j,0;0,1")),
+    "Z": np.array(np.mat("1+0j,0;0,-1")),
+    "X": np.array(np.mat("0+0j,1;1,0")),
+    "Y": np.array(np.mat("0,-1j;1j,0")),
 }
 #%%
 # init all the tensors
@@ -53,7 +55,7 @@ def gen_graph(n=2):
 
 
 #%%
-n = 4
+n = 2
 G = gen_graph(n)
 G.remove_node("".join("I" for i in range(n)))
 print(nx.algorithms.approximation.large_clique_size(G))
@@ -74,15 +76,80 @@ check=0
 
 max_xs=nx.algorithms.approximation.large_clique_size(G)
 for i in nx.find_cliques(G):
-    if len(i) == 3:#max_xs:
+    if len(i) == 5:#max_xs:
         cliques.append(set(i))
 
 for i in nx.find_cliques(G):
     if not any([set(i).issubset(b) for b in cliques]):
-        if len(i) == 3:
+        if len(i) == 7:
             num_cliq = num_cliq +1
 
-print(num_cliq,len(cliques))
+print(len(cliques))
+#%%
+def gen_paulis(lis):
+    """
+
+    Parameters
+    ----------
+    lis : list
+        list of the form ['xyz','xxi'...] so that each the first char in
+        each element of the list is pauli corresponding to the char.
+
+    Returns
+    -------
+    paul_lis : lis
+        [[pauli(x),pauli(y) pauli(z)],].
+
+    """
+    pl=[[pauli[j] for j in i] for i in list(lis)]
+    pauli_lis = [[row[i] for row in pl] for i in range(len(list(lis)[0]))]
+    return pauli_lis
+
+def multiply_lis(lis):
+    asd = [functools.reduce(np.dot, i) for i in lis]
+    return asd
+
+def find_pauli(lis):
+    
+    st=''
+    for i in lis:
+        if i[0][0]!=0:
+            if i[0][0]+i[1][1]==0:
+                st=st+'Z'
+            else:
+                st=st+'I'
+        elif i[0][1]+i[1][0]==0:
+            st=st+'Y'
+        else:
+            st=st+'X'
+    return st
+    
+for i in itertools.combinations(cliques[0],2):
+    chk = copy.copy(cliques[0])
+    for j in i: chk.remove(j)
+    print(i,find_pauli(multiply_lis(gen_paulis(chk))))
+    
+#%%sub cliques
+basis_sets=[]
+count=0
+for i in cliques:
+#pick 2
+    for j in itertools.combinations(i,2):
+        count+=1
+        chk = copy.copy(i)
+        for k in j: chk.remove(k)
+        se=set(j)
+        se.add(find_pauli(multiply_lis(gen_paulis(chk))))
+        
+        if not any([se.issubset(b) for b in basis_sets]):
+            basis_sets.append(se)
+#multiply the rest
+        #gen_paulis
+        #multiply
+        #find pauli return to 'x''y'z' etc
+
+
+
 #%% count max identity
 max_xs=nx.algorithms.approximation.large_clique_size(G)
 for b in nx.find_cliques(G):
